c787dcec8714400cbc15025fd18ea85b2bab758c
ludinj <borrego300@yahoo.com>
2022-05-13 13:16:57 -0400

    minor chages in index.js

--- src/index.js
+++ src/index.js
@@ -4,5 +4,4 @@ import React from "react";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(<App name="Saeloun blog" />);
 root.render(<App />);
